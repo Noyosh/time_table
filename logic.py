5bd783264d33c5c81b9e00093cb145927976251e
jbt <noya.weinberg6340@gmail.com>
2024-09-19 14:38:15 +0300

    final without מקרי קצה

--- logic.py
+++ logic.py
@@ -6,28 +6,28 @@ FREE = "free"
 DAY_LIST = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday"]
 WEEK_DAY = {"sunday": 0, "monday": 1, "tuesday": 2, "wednesday": 3, "thursday": 4, "friday": 5}
 
-HOURS = input("how many hours are in the schedule?")
-while not HOURS.isnumeric or int(HOURS) > 8 or int(HOURS) < 0:
-    print("please enter a number")
-    HOURS = input("how many hours are in the schedule?")
-HOURS = int(HOURS)
-
-DAYS = (input("how many days are in the schedule?"))
-while not DAYS.isnumeric or int(DAYS) > 6 or int(DAYS) < 0:
-    print("please enter a number")
-    DAYS = (input("how many days are in the schedule?"))
-DAYS = int(DAYS)
-
-CLASSES_NUM = input("please enter the amount of classes")
+CLASSES_NUM = input("please enter the num of classes")
 while not CLASSES_NUM.isnumeric():
     print("please enter a number")
-    CLASSES_NUM = input("please enter the amount of classes")
+    CLASSES_NUM = input("please enter the num of classespip")
 CLASSES_NUM = int(CLASSES_NUM)
+
+HOURS = input("Please enter the number of study hours per day between 1-8")
+while not HOURS.isnumeric or int(HOURS) > 8 or int(HOURS) < 0:
+
+    HOURS = input("Please enter the number of study hours per day between 1-8")
+HOURS = int(HOURS)
+
+DAYS = (input("Please enter the number of school days per week between 1-8"))
+while not DAYS.isnumeric or int(DAYS) > 6 or int(DAYS) < 0:
+
+    DAYS = (input("Please enter the number of school days per week between 1-8"))
+DAYS = int(DAYS)
 
 TOTAL_SCH_HOUR = HOURS * DAYS
 
 SUBJECT_NAME_DICT = {}
-SUBJECT_NAME = input("please enter subject name")
+SUBJECT_NAME = input("Enter the name of the lesson here, enter done to stop")
 
 NUM_OF_LESSON_HOURS = input("please enter how many hours for the subject ")
 
@@ -36,7 +36,7 @@ SUM_OF_LESSON_HOURS = 0
 while SUBJECT_NAME != "done":
     while not NUM_OF_LESSON_HOURS.isnumeric():
         print("please enter a number")
-        NUM_OF_LESSON_HOURS = input("please enter a num for hours")
+        NUM_OF_LESSON_HOURS = input("Enter the number of study hours for the lesson here")
 
     NUM_OF_LESSON_HOURS = int(NUM_OF_LESSON_HOURS)
 
@@ -45,7 +45,7 @@ while SUBJECT_NAME != "done":
         SUM_OF_LESSON_HOURS -= NUM_OF_LESSON_HOURS
         print("please enter a lower amount of hours for the"
               " lesson this is how many left:", TOTAL_SCH_HOUR - SUM_OF_LESSON_HOURS)
-        NUM_OF_LESSON_HOURS = input("please enter how many hours for the subject ")
+        NUM_OF_LESSON_HOURS = input("Enter the number of study hours for the lesson here")
 
     elif SUM_OF_LESSON_HOURS == TOTAL_SCH_HOUR:
         SUBJECT_NAME_DICT[SUBJECT_NAME] = NUM_OF_LESSON_HOURS
@@ -54,9 +54,9 @@ while SUBJECT_NAME != "done":
 
     else:
         SUBJECT_NAME_DICT[SUBJECT_NAME] = NUM_OF_LESSON_HOURS
-        SUBJECT_NAME = input("please enter subject name")
+        SUBJECT_NAME = input("Enter the name of the lesson here, enter done to stop")
         if not SUBJECT_NAME == "done":
-            NUM_OF_LESSON_HOURS = input("please enter how many hours for the subject ")
+            NUM_OF_LESSON_HOURS = input("Enter the number of study hours for the lesson here")
 
 print(SUBJECT_NAME_DICT)
 
@@ -67,9 +67,9 @@ TEACHER_BUSY_DAYS = {}
 
 for name in SUBJECT_NAME_LIST:
     print(name)
-    TEACHER_NUM = input("how many teacher are for this subject")
+    TEACHER_NUM = input("Enter here how many teachers teach this lesson")
     while not TEACHER_NUM.isnumeric():
-        TEACHER_NUM = input("how many teacher are for this subject")
+        TEACHER_NUM = input("Enter here how many teachers teach this lesson")
     TEACHER_NUM = int(TEACHER_NUM)
 
     for num in range(TEACHER_NUM):
@@ -84,7 +84,7 @@ for name in SUBJECT_NAME_LIST:
                 print("please enter a currect name")
             else:
                 BUSY_DAYS_LIST.append(WEEK_DAY[BUSY_DAYS])
-                BUSY_DAYS = input("please enter which days you cannot work or done")
+                BUSY_DAYS = input("please enter which days you cannot work or enter done to stop")
                 NUM_OF_DAYS += 1
 
                 for i in list(TEACHER_BUSY_DAYS.values()):
@@ -103,8 +103,7 @@ for name in SUBJECT_NAME_LIST:
                 file = open("teacher_" + name + "_" + str(num) + ".txt", 'w')
                 TEACHER_LIST.append("teacher_" + name + "_" + str(num) + ".txt")
                 json.dump(S, file)
-            BUSY_DAYS = input("please enter which days you cannot work or done")
-
+            BUSY_DAYS = input("please enter which days you cannot work or enter done to stop")
 
 CLASS_NAME_LIST = []
 
@@ -125,9 +124,8 @@ for c in range(CLASSES_NUM):
                             sch_teacher[day][hour] = BUSY
                             file = open(teacher, 'w')
                             json.dump(sch_teacher, file)
-                            S[day][hour] = name + teacher
+                            S[day][hour] = teacher
                             subject_full += 1
     file = open("class" + str(c) + ".txt", 'w')
     CLASS_NAME_LIST.append("class" + str(c) + ".txt")
     json.dump(S, file)
-
