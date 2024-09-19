8c0073c20c2b14096010269a75c6151f291dd4ad
jbt <noya.weinberg6340@gmail.com>
2024-09-19 14:24:23 +0300

    final without מקרי קצה

--- logic.py
+++ logic.py
@@ -6,32 +6,58 @@ FREE = "free"
 DAY_LIST = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday"]
 WEEK_DAY = {"sunday": 0, "monday": 1, "tuesday": 2, "wednesday": 3, "thursday": 4, "friday": 5}
 
-HOURS = int(input("how many hours are in the schedule?"))
-DAYS = int(input("how many days are in the schedule?"))
-CLASSES_NUM = int(input("please enter the amount of classes"))
+HOURS = input("how many hours are in the schedule?")
+while not HOURS.isnumeric or int(HOURS) > 8 or int(HOURS) < 0:
+    print("please enter a number")
+    HOURS = input("how many hours are in the schedule?")
+HOURS = int(HOURS)
+
+DAYS = (input("how many days are in the schedule?"))
+while not DAYS.isnumeric or int(DAYS) > 6 or int(DAYS) < 0:
+    print("please enter a number")
+    DAYS = (input("how many days are in the schedule?"))
+DAYS = int(DAYS)
+
+CLASSES_NUM = input("please enter the amount of classes")
+while not CLASSES_NUM.isnumeric():
+    print("please enter a number")
+    CLASSES_NUM = input("please enter the amount of classes")
+CLASSES_NUM = int(CLASSES_NUM)
 
 TOTAL_SCH_HOUR = HOURS * DAYS
 
 SUBJECT_NAME_DICT = {}
 SUBJECT_NAME = input("please enter subject name")
-NUM_OF_LESSON_HOURS = int(input("please enter how many hours for the subject "))
+
+NUM_OF_LESSON_HOURS = input("please enter how many hours for the subject ")
+
 SUM_OF_LESSON_HOURS = 0
 
 while SUBJECT_NAME != "done":
+    while not NUM_OF_LESSON_HOURS.isnumeric():
+        print("please enter a number")
+        NUM_OF_LESSON_HOURS = input("please enter a num for hours")
+
+    NUM_OF_LESSON_HOURS = int(NUM_OF_LESSON_HOURS)
+
     SUM_OF_LESSON_HOURS += NUM_OF_LESSON_HOURS
     if SUM_OF_LESSON_HOURS > TOTAL_SCH_HOUR:
         SUM_OF_LESSON_HOURS -= NUM_OF_LESSON_HOURS
         print("please enter a lower amount of hours for the"
               " lesson this is how many left:", TOTAL_SCH_HOUR - SUM_OF_LESSON_HOURS)
-        NUM_OF_LESSON_HOURS = int(input("please enter how many hours for the subject "))
+        NUM_OF_LESSON_HOURS = input("please enter how many hours for the subject ")
+
     elif SUM_OF_LESSON_HOURS == TOTAL_SCH_HOUR:
         SUBJECT_NAME_DICT[SUBJECT_NAME] = NUM_OF_LESSON_HOURS
         print("the schedule is full")
         SUBJECT_NAME = "done"
+
     else:
         SUBJECT_NAME_DICT[SUBJECT_NAME] = NUM_OF_LESSON_HOURS
         SUBJECT_NAME = input("please enter subject name")
-        NUM_OF_LESSON_HOURS = int(input("please enter how many hours for the subject "))
+        if not SUBJECT_NAME == "done":
+            NUM_OF_LESSON_HOURS = input("please enter how many hours for the subject ")
+
 print(SUBJECT_NAME_DICT)
 
 SUBJECT_NAME_LIST = list(SUBJECT_NAME_DICT.keys())
@@ -41,7 +67,11 @@ TEACHER_BUSY_DAYS = {}
 
 for name in SUBJECT_NAME_LIST:
     print(name)
-    TEACHER_NUM = int(input("how many teacher are fot this subject"))
+    TEACHER_NUM = input("how many teacher are for this subject")
+    while not TEACHER_NUM.isnumeric():
+        TEACHER_NUM = input("how many teacher are for this subject")
+    TEACHER_NUM = int(TEACHER_NUM)
+
     for num in range(TEACHER_NUM):
         BUSY_DAYS_LIST = []
         NUM_OF_DAYS = 0
@@ -50,26 +80,31 @@ for name in SUBJECT_NAME_LIST:
         BUSY_DAYS = input("please enter which days you cannot work or done")
 
         while BUSY_DAYS != "done":
-            BUSY_DAYS_LIST.append(WEEK_DAY[BUSY_DAYS])
-            BUSY_DAYS = input("please enter which days you cannot work or done")
-            NUM_OF_DAYS += 1
-
-        for i in list(TEACHER_BUSY_DAYS.values()):
-            if NUM_OF_DAYS > i:
-                TEACHER_BUSY_DAYS_NEW = {"teacher" + name + str(num) + ".txt": NUM_OF_DAYS}
-                TEACHER_BUSY_DAYS_NEW.update(TEACHER_BUSY_DAYS)
-                TEACHER_BUSY_DAYS = TEACHER_BUSY_DAYS_NEW
+            if BUSY_DAYS.isnumeric or (not (BUSY_DAYS in DAY_LIST)):
+                print("please enter a currect name")
             else:
-                TEACHER_BUSY_DAYS["teacher" + name + str(num) + ".txt"] = NUM_OF_DAYS
+                BUSY_DAYS_LIST.append(WEEK_DAY[BUSY_DAYS])
+                BUSY_DAYS = input("please enter which days you cannot work or done")
+                NUM_OF_DAYS += 1
 
-        S = [[FREE for x in range(HOURS)] for i in range(DAYS)]
-        for i in range(len(S)):
-            if i in BUSY_DAYS_LIST:
-                for j in range(len(S[i])):
-                    S[i][j] = BUSY
-        file = open("teacher_" + name + "_" + str(num) + ".txt", 'w')
-        TEACHER_LIST.append("teacher_" + name + "_" + str(num) + ".txt")
-        json.dump(S, file)
+                for i in list(TEACHER_BUSY_DAYS.values()):
+                    if NUM_OF_DAYS > i:
+                        TEACHER_BUSY_DAYS_NEW = {"teacher" + name + str(num) + ".txt": NUM_OF_DAYS}
+                        TEACHER_BUSY_DAYS_NEW.update(TEACHER_BUSY_DAYS)
+                        TEACHER_BUSY_DAYS = TEACHER_BUSY_DAYS_NEW
+                    else:
+                        TEACHER_BUSY_DAYS["teacher" + name + str(num) + ".txt"] = NUM_OF_DAYS
+
+                S = [[FREE for x in range(HOURS)] for i in range(DAYS)]
+                for i in range(len(S)):
+                    if i in BUSY_DAYS_LIST:
+                        for j in range(len(S[i])):
+                            S[i][j] = BUSY
+                file = open("teacher_" + name + "_" + str(num) + ".txt", 'w')
+                TEACHER_LIST.append("teacher_" + name + "_" + str(num) + ".txt")
+                json.dump(S, file)
+            BUSY_DAYS = input("please enter which days you cannot work or done")
+
 
 CLASS_NAME_LIST = []
 
@@ -96,10 +131,3 @@ for c in range(CLASSES_NUM):
     CLASS_NAME_LIST.append("class" + str(c) + ".txt")
     json.dump(S, file)
 
-#         TEACHER_LIST.append("teacher" + name + str(num)+".txt")
-#         S = [[FREE for x in range(HOURS)] for i in range(DAYS)]
-#         file = open("teacher" + name + str(num) + ".txt", 'w')
-#         json.dump(S, file)
-#
-#
-#
